f241f21d907401bf6bfe7196bb48b0b51ec323c7
kurrbanov <justnick1312@gmail.com>
2022-06-11 01:30:24 +0300

    register Customer model

--- admin_panel/admin.py
+++ admin_panel/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from admin_panel.models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'email')
+
