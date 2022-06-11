329c34cc01867f8d0be22eadd02d4e2ce0fc87bf
kurrbanov <justnick1312@gmail.com>
2022-06-11 16:54:05 +0300

    changes the verbose names of models and created indexes

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -10,6 +10,10 @@ class Customer(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+    class Meta:
+        verbose_name = 'Заказчик'
+        verbose_name_plural = 'Заказчики'
+
 
 class Order(models.Model):
     STATUSES = (
@@ -22,25 +26,40 @@ class Order(models.Model):
     )
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='Закзчик')
     address = models.TextField(verbose_name='Адрес доставки')
-    status = models.CharField(choices=STATUSES, max_length=12, verbose_name='Статус заказа')
+    status = models.CharField(choices=STATUSES, max_length=12, verbose_name='Статус заказа', default='Создан')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
 
     def __str__(self):
         return f"#{self.id}: {self.status}"
 
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+        indexes = [
+            models.Index(fields=['status', 'created_at'])
+        ]
+
 
 class Product(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Название продукта')
+    title = models.CharField(max_length=255, verbose_name='Название товара')
 
     def __str__(self):
         return f"{self.title}"
 
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
 
 class OrderItem(models.Model):
     order_id = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Номер заказа')
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Название продукта')
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Название товара')
     quantity = models.IntegerField(verbose_name='Количество', validators=[MinValueValidator(1)])
     price = models.FloatField(verbose_name='Цена', validators=[MinValueValidator(0)])
 
     def __str__(self):
         return f"{self.product_id} - {self.price}"
+
+    class Meta:
+        verbose_name = 'Позиция заказа'
+        verbose_name_plural = 'Позиции заказа'
